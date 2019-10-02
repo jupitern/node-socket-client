1e6ac33896cecb516a60862e76e76e38e49043e1
TCB13 <tcb13@iklive.eu>
2019-10-02 17:38:20 +0100

    Don't start all connection at the same time
    Avoids crashing the client (and the server?)

--- client.js
+++ client.js
@@ -1,45 +1,65 @@
-var WebSocketClient = require('websocket').client;
+var desiredConnections = 50000;
+var serverUrl = 'ws://142.93.131.241/chatserver';
 
+var WebSocketClient = require('websocket').client;
 var connectionsCount = 0;
 
-for (var i=0; i<6000; ++i) {
-
-    var client = new WebSocketClient();
-
-    client.on('connectFailed', function(error) {
-        console.log('Connect Error: ' + error.toString());
-    });
-
-    client.on('connect', function(connection) {
-        console.log('Client Connected. total = '+ connectionsCount);
-        connectionsCount++;
-
-        connection.on('error', function(error) {
-            connectionsCount--;
-            connection.disconnect();
-            console.log("Connection Error: " + error.toString()+' total = '+ connectionsCount);
-        });
-
-        connection.on('close', function() {
-            connectionsCount--;
-            console.log("Connection Closed. total = "+ connectionsCount);
-        });
-
-        connection.on('message', function(message) {
-            if (message.type === 'utf8') {
-                console.log("Received: '" + message.utf8Data + "'");
-            }
-        });
-
-        // function sendNumber() {
-        //     if (connection.connected) {
-        //         var number = Math.round(Math.random() * 0xFFFFFF);
-        //         connection.sendUTF(number.toString());
-        //         setTimeout(sendNumber, 1000);
-        //     }
-        // }
-        // sendNumber();
-    });
-
-    client.connect('ws://68.183.8.119:8081/');
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
+
+function connect() {
+   var client = new WebSocketClient();
+
+   client.on('connectFailed', function(error) {
+        console.log('Connect Error: ' + error.toString());
+   });
+
+   client.on('connect', function(connection) {
+    console.log('Client Connected. total = '+ connectionsCount);
+    connectionsCount++;
+
+    connection.on('error', function(error) {
+        connectionsCount--;
+        connection.disconnect();
+        console.log("Connection Error: " + error.toString()+' total = '+ connectionsCount);
+    });
+
+    connection.on('close', function() {
+        connectionsCount--;
+        console.log("Connection Closed. total = "+ connectionsCount);
+    });
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+
+    // function sendNumber() {
+    //     if (connection.connected) {
+    //         var number = Math.round(Math.random() * 0xFFFFFF);
+    //         connection.sendUTF(number.toString());
+    //         setTimeout(sendNumber, 1000);
+    //     }
+    // }
+    // sendNumber();
+
+    if (desiredConnections > connectionsCount) {
+        /*if (connectionsCount % 100 == 0) {
+            sleep(200).then(() => {
+                connect();
+            });
+        } else {
+            connect();
+        }*/
+        connect();
+    }
+
+    });
+
+    client.connect(serverUrl);
+
+}
+
+connect();
